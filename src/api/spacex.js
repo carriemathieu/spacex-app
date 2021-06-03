7f2786e68b5138c410d4eabbec14f710d1b16233
Carrie Mathieu <carrieburks@gmail.com>
2021-06-02 22:20:11 -0400

    created launchcard/launchlist components

---
+++ src/api/spacex.js
@@ -0,0 +1,6 @@
+import axios from 'axios'
+
+// create instance of axios client w/ couple default props
+export const spacexLaunches = () => {
+    return axios.get('https://api.spacexdata.com/v4 ')
+}
