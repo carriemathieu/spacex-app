2a56c69660e833e2c8f0ae3205f4be96fe2651f0
Carrie Mathieu <carrieburks@gmail.com>
2021-06-03 10:45:33 -0400

    launchcard comments

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import axios from 'axios'
+// import axios from 'axios'
 import { LaunchList } from './Components/LaunchList'
 
 import './App.css';
--- src/Components/LaunchCard.js
+++ src/Components/LaunchCard.js
@@ -1,1 +1,10 @@
+import axios from 'axios'
 import React from 'react'
+
+
+// when user clicks on hyperlink, redirects to presskit for that launch using the URL provided in the API response
+// onclick(id) {
+//     axios.get('https://api.spacexdata.com/v4', {
+//         params: { query: id }
+//     })
+// }
