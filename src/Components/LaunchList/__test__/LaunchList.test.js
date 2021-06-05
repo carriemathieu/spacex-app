a7caa393252473095bf2daa441a97f6bc8944bb4
Carrie Mathieu <carrieburks@gmail.com>
2021-06-05 18:36:59 -0400

    modified tests

--- src/Components/LaunchList/__test__/LaunchList.test.js
+++ src/Components/LaunchList/__test__/LaunchList.test.js
@@ -5,36 +5,39 @@ import LaunchList from '../LaunchList'
 import { render, fireEvent } from "@testing-library/react"
 
 // allows us to use extend keyword
-import "@testing-library/jest-dom/extend-expect"
+import '@testing-library/jest-dom/extend-expect'
 
-// globab variable so functions can access
-let getByTestId
+// global variable so functions can access destructured getByTestId
+let getByTestId;
 
 // executes before each test is run
 beforeEach(() => {
     const component = render(<LaunchList/>)
-    getByTestId = component.getByTestID
+    getByTestId = component.getByTestId
 })
 
 // first param string describing what test is doing
 // second param is function that contains logic for test
-test("header renders with correct test", () => {
-    // destructures "component" to be able to use getByTestID 
+test("page header renders with correct test", () => {
     const headerEl = getByTestId("header")
 
     // "expect" allows us to test for specific item
-    expect(headerEl.textContent).toBe("LaunchList")
+    expect(headerEl.textContent).toBe("SpaceX Launches")
 })
 
 // tests if launch list column headers are displayed on page
-test("displays columns: Flight Number, Launch Year, Rocket Name, and Details", () => {
-    // const launchesEl = getByTestId("launches")
+test("displays column headers correctly: Flight Number, Launch Year, Rocket Name, and Details", async () => {
+    // destructures "component" to be able to use findByTestID 
+    const { findAllByTestId } = render(<LaunchList/>)
 
-    // expect(launchesEl.textContent).toBe(" ")
+    const tableHeaders = await findAllByTestId("table-header")
+
+    // *** would like to do more research on using waitFor in react testing in order to test for array length ***
+    expect(tableHeaders).not.toContain('')
 })
 
 test("API results are sorted in launch order", () => {
-
+    // expect(isAscending(element)).toBe(true)
 })
 
 test("row links to presskit", () => {
