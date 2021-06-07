9d97babfe088933fa873f0d28a9d5fd53eb31a7e
Carrie Mathieu <carrieburks@gmail.com>
2021-06-07 14:41:00 -0400

    updated issue with pagination

--- src/Components/Pagination/Pagination.js
+++ src/Components/Pagination/Pagination.js
@@ -17,7 +17,7 @@ const Pagination = ({ launchesPerPage, totalLaunches, paginate }) => {
             <ul className="pagination" data-testid="pagination">
                 {pageNumbers.map(number => {
                     return <li key={number} className='page-item'>
-                        <a onClick={() => paginate(number)} href='!#' className='page-link'>
+                        <a onClick={() => paginate(number)}  className='page-link'>
                             {number}
                         </a>
                     </li>
